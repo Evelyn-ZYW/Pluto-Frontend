41d456efb701e76962f7ae3a16fa4277fd47517b
Virginie Soulodre-Whan <virginiesw@gmail.com>
2021-03-24 17:10:07 -0700

    profile 1/2 complete.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import ImgButton from '../../comps/ImgButton';
 import Profile from '../../comps/Profile';
@@ -8,6 +8,7 @@ import Avatar from '../../assets/a1.png';
 import Cute1 from '../../assets/pomer.jpg';
 import Cute2 from '../../assets/cute.jpeg';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 const Container = styled.div`
 border: 1px solid black;
@@ -53,16 +54,61 @@ const Bottom = styled.div`
 
 const ProfilePage = () => {
     const history = useHistory();
+
+    const [ui, setUI] = useState();
+    const [un, setUn] = useState();
+    const [profile, setProfile] = useState([]);
+    const [pui, setPUI] = useState();
+
     const EditProfile = () => {
         history.push('/EditProfile')
     }
+
+    const GetProfile = async () => {
+        const resp = await axios.post("http://localhost:8080/api/getOneProfile", {id: ui});
+        console.log(resp.data);
+
+
+            // setProfile([
+            //     ...resp.data.user
+            // ]);
+            // console.log(resp.data., "users")
+        }  
+
+
+
+
+    const CheckToken = async () => {
+        //asume we will store the token in the sessionStorage
+        const token = await sessionStorage.getItem("token");
+        console.log("token", token)
+        axios.defaults.headers.common['Authorization'] = token;
+
+        function parseJwt(token) {
+            if (!token) { return; }
+            const base64Url = token.split('.')[1];
+            const base64 = base64Url.replace('-', '+').replace('_', '/');
+            return JSON.parse(window.atob(base64));
+        }
+        const data = parseJwt(token);
+        const userId = data.userId
+        const username = data.username
+        console.log("this is the userId", userId)
+        setUI(userId)
+        setUn(username)
+    }
+
+    useEffect(() => {
+        CheckToken();
+        GetProfile();
+    }, [])
     return <Container>
         <Top>
-            <h3 className="uname">Username</h3>
+            <h3 className="uname">{un}</h3>
             <ImgButton src={More} maxwh="27px" maxht="27px" onClick={EditProfile} />
         </Top>
         <Middle>
-            <Profile src={Avatar} />
+            <Profile username={un} src={Avatar} />
         </Middle>
         <Bottom className="bottom">
             <div><img src={Cute1} /></div>
